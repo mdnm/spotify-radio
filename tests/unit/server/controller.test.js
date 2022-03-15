3c098dbc8a08662d7d0a640fd7d23cea6140b9c4
Mateus De Nardo <mateusdnm@hotmail.com>
2022-03-15 03:52:35 +0000

    feat: add static file serving and tests

---
+++ tests/unit/server/controller.test.js
@@ -0,0 +1,33 @@
+import { beforeEach, describe, expect, jest, test } from '@jest/globals'
+import { Controller } from '../../../server/controller.js'
+import { Service } from '../../../server/service.js'
+import TestUtil from '../_util/testUtil.js'
+
+describe("#Controller - test suite for API control", () => {
+  beforeEach(() => {
+    jest.restoreAllMocks()
+    jest.clearAllMocks()
+  })
+
+  test('getFileStream - should return a file stream', async () => {
+    const mockFileStream = TestUtil.generateReadableStream(['data'])
+    const expectedType = '.html'
+
+    jest.spyOn(
+      Service.prototype,
+      Service.prototype.getFileStream.name,
+    ).mockResolvedValue({
+      stream: mockFileStream,
+      type: expectedType
+    })
+
+    const controller = new Controller()
+    const controllerReturn = await controller.getFileStream('/index.html')
+
+    expect(Service.prototype.getFileStream).toBeCalledWith('/index.html')
+    expect(controllerReturn).toStrictEqual({
+      stream: mockFileStream,
+      type: expectedType
+    })
+  })
+})
---
+++ tests/unit/server/service.test.js
@@ -0,0 +1,88 @@
+import { beforeEach, describe, expect, jest, test } from '@jest/globals';
+import fs from 'fs';
+import fsPromises from 'fs/promises';
+import path from 'path';
+import config from '../../../server/config.js';
+import { Service } from '../../../server/service.js';
+import TestUtil from '../_util/testUtil.js';
+
+const {
+  dir: {
+    publicDirectory
+  }
+} = config
+
+describe("#Service - test suite for API service", () => {
+  beforeEach(() => {
+    jest.restoreAllMocks()
+    jest.clearAllMocks()
+  })
+
+  test('createFileStream - should create a file stream and return it', async () => {
+    const file = '/index.html'
+    const mockFileStream = TestUtil.generateReadableStream(['data'])
+
+    jest
+      .spyOn(fs, fs.createReadStream.name)
+      .mockResolvedValue(mockFileStream)
+
+    const service = new Service()
+    const serviceReturn = service.createFileStream(file)
+
+    expect(fs.createReadStream).toBeCalledWith(file)
+    expect(serviceReturn).resolves.toStrictEqual(mockFileStream)
+  })
+  test('getFileInfo - should return file info', async () => {
+    const file = '/index.html'
+    const expectedType = '.html'
+    const expectedFullFilePath = publicDirectory + file;
+
+    jest
+      .spyOn(path, path.join.name)
+      .mockResolvedValue(expectedFullFilePath)
+
+    jest
+      .spyOn(fsPromises, fsPromises.access.name)
+      .mockResolvedValue(null)
+
+    jest
+      .spyOn(path, path.extname.name)
+      .mockResolvedValue('.html')
+
+    const service = new Service()
+    const serviceReturn = await service.getFileInfo(file)
+
+    expect(serviceReturn).toStrictEqual({
+      type: expectedType,
+      name: expectedFullFilePath
+    })
+  })
+  test('getFileStream - should create a file stream and return it with the file type', async () => {
+    const file = '/index.html'
+    const expectedType = '.html'
+    const expectedFullFilePath = publicDirectory + file;
+    const mockFileStream = TestUtil.generateReadableStream(['data'])
+
+    jest
+      .spyOn(fs, fs.createReadStream.name)
+      .mockResolvedValue(mockFileStream)
+
+    jest
+      .spyOn(path, path.join.name)
+      .mockResolvedValue(expectedFullFilePath)
+
+    jest
+      .spyOn(fsPromises, fsPromises.access.name)
+      .mockResolvedValue(null)
+
+    jest
+      .spyOn(path, path.extname.name)
+      .mockResolvedValue('.html')
+
+    const service = new Service()
+    const serviceReturn = await service.getFileStream(file)
+
+    expect(serviceReturn.stream).resolves.toStrictEqual(mockFileStream)
+    expect(serviceReturn.type).toStrictEqual(expectedType)
+  })
+})
